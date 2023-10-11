65d3c3f18020b0c6c76c29c42b7c58a6feb437db
Igor Leonovich <8408481+igorleonovich@users.noreply.github.com>
2023-10-11 06:28:16 +0400

    Tests: Create FiltersViewModelTests

---
+++ VideoMerger-Apple-Tests/FiltersViewModelTests/DummyResponse.swift
@@ -0,0 +1,11 @@
+//
+//  DummyResponse.swift
+//  FiltersViewModelTests
+//
+//  Created by Igor Leonovich on 11/10/2023.
+//
+
+import UIKit
+
+@testable import VideoMerger_Apple
+
--- VideoMerger-Apple-Tests/FiltersViewModelTests/FiltersViewModelTests.swift
+++ VideoMerger-Apple-Tests/FiltersViewModelTests/FiltersViewModelTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class VideoMerger_Apple_Tests: XCTestCase {
+final class FiltersViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
